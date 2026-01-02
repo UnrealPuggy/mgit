003e560083a563ab108593a443780f67c1120f57
UnrealPuggy <126524020+UnrealPuggy@users.noreply.github.com>
2026-01-02 14:42:56 -0700

    Revert "initial commit"
    This reverts commit a0f837fd3b4c5f6f32c10f515d01b08bcdc4a36e.

--- src/repo/paths.ts
+++
@@ -1,5 +0,0 @@
-export const gitDir = ".mgit/";
-export const objDir = gitDir + "object/";
-export const headPath = gitDir + "HEAD";
-export const indexPath = gitDir + "index";
-export const branchDir = "refs/heads/";
